699077a685816ce7469568c5bcab57f64c65d070
Leo mwahalende <166632518+Mwahalende@users.noreply.github.com>
2025-07-23 10:18:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const Comment = require('./models/Comment');
 passport.use(new GoogleStrategy({
   clientID: '199468518160-0ubjlcvm41a5gk0ud78fkpfuu5kn0v7a.apps.googleusercontent.com',
   clientSecret: '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',
-  callbackURL: 'https://saints-history-and-photo.onrender.com'
+  callbackURL: 'https://saint-platform.onrender.com/media.html'
 }, async (accessToken, refreshToken, profile, done) => {
   let admin = await Admin.findOne({ googleId: profile.id });
   if (!admin) {
